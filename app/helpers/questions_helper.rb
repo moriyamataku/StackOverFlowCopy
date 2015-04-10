e01bfc4ef42da0d13566f3f4d03fac12fc3f9806
Akihiro Matsumura <matsumura.aki@gmail.com>
2015-04-10 20:33:55 +0900

    DRY
    I18n を活用

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,19 @@
 module QuestionsHelper
+  def answers_count_text(question)
+    content_tag(:div, class: "mini-counts") do
+      question.get_answers_count
+    end + content_tag(:div) do
+      '回答'
+    end
+  end
+
+  def question_answer_count_class(question)
+    if question.get_answers_count == 0
+      ''
+    elsif question.solved
+      'answered-accepted'
+    else
+      'answered'
+    end
+  end
 end
